1dad950663d552b1ac7713f28244c5990d059441
Madhan Vasudevan <madhaniyer@gmail.com>
2020-05-16 21:24:40 +1200

    Initial Commit

---
+++ Covid19 Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid19 Tracker
+//
+//  Created by Madhan Vasudevan on 16/05/20.
+//  Copyright © 2020 redwine. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Covid19 TrackerTests/Covid19_TrackerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Covid19_TrackerTests.swift
+//  Covid19 TrackerTests
+//
+//  Created by Madhan Vasudevan on 16/05/20.
+//  Copyright © 2020 redwine. All rights reserved.
+//
+
+import XCTest
+@testable import Covid19_Tracker
+
+class Covid19_TrackerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
